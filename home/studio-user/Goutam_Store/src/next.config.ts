444f4675f1fd08bf95676c0178a0f536f2b356a0
Subham Ranjan <subham8050paswan@gmail.com>
2025-08-10 21:17:06 +0000

    26s Run FirebaseExtended/action-hosting-deploy@v0 Verifying firebase.jso

---
+++ home/studio-user/Goutam_Store/src/next.config.ts
@@ -0,0 +1,50 @@
+
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  /* config options here */
+  devIndicators: {
+    allowedDevOrigins: ['*'],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'goutam-store-3uiby.appspot.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
+
+
