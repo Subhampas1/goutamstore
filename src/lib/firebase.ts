b07da08736a77080e1969045c1bdf337d118a776
Subham Ranjan <subham8050paswan@gmail.com>
2025-08-10 05:47:02 +0000

    are you using these credentials for firebase "// Import the functions yo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,21 +6,16 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "goutam-store-3uiby",
-  "appId": "1:991543455116:web:2a2e0eda4fc3e34bfd3aa9",
-  "storageBucket": "goutam-store-3uiby.appspot.com",
-  "apiKey": "AIzaSyAlO7GrnMDubwsc7GrM2J3a6E8LP7qrGjc",
-  "authDomain": "goutam-store-3uiby.firebaseapp.com",
-  "messagingSenderId": "991543455116"
+  apiKey: "AIzaSyAlO7GrnMDubwsc7GrM2J3a6E8LP7qrGjc",
+  authDomain: "goutam-store-3uiby.firebaseapp.com",
+  projectId: "goutam-store-3uiby",
+  storageBucket: "goutam-store-3uiby.appspot.com",
+  messagingSenderId: "991543455116",
+  appId: "1:991543455116:web:2a2e0eda4fc3e34bfd3aa9"
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
